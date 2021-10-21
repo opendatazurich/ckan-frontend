564dfa0cfedc592cba7ef4d89061e4b9985a6147
Sascha Aeppli <sascha.aeppli@liip.ch>
2021-10-21 02:53:01 +0200

    file labels

---
+++ src/lib/DatasetList.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import FileFormats from './FileFormats.svelte';
+
+	export let datasets = [];
+</script>
+
+<ul class="dataset-list unstyled">
+	{#each datasets as dataset}
+		<li class="dataset-item">
+			<div class="dataset-content">
+				<h3 class="dataset-heading">
+					<a href="/dataset/{dataset.name}">{dataset.title}</a>
+				</h3>
+				<div>
+					{dataset.notes}
+				</div>
+			</div>
+
+			<FileFormats href="/dataset/{dataset.name}" resources={dataset.resources} />
+		</li>
+	{/each}
+</ul>
---
+++ src/lib/FileFormats.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let href: string;
+	export let resources: { format: string }[];
+	const formats = Object.keys(
+		resources
+			.map((resource) => resource.format.toLocaleLowerCase())
+			.reduce((obj, format) => {
+				obj[format] = true;
+				return obj;
+			}, {})
+	);
+</script>
+
+<ul class="dataset-resources unstyled">
+	{#each formats as format}
+		<li>
+			<a {href} class="label" data-format={format}>{format}</a>
+		</li>
+	{/each}
+</ul>
