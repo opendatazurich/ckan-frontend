a047e7447e41afcf88f83ff3a7d1cb16f488b042
Sascha Aeppli <sascha.aeppli@liip.ch>
2021-11-09 17:08:29 +0100

    added types description. api button. accordion component.

---
+++ src/lib/ResourceList.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { truncate } from './string';
+
+	export let dataset: { name: string; resources: any[] };
+	export let resource;
+</script>
+
+<section class="module module-narrow resources">
+	<h2 class="module-heading"><i class="fa fa-files-o" /> Ressourcen</h2>
+
+	<ul class="unstyled nav nav-simple">
+		{#each dataset.resources as r}
+			<li class="nav-item" class:active={r === resource}>
+				<a href="/dataset/{dataset.name}/resource/{r.id}"
+					><span>{truncate(r.name, 22, ' ...', false)}</span></a
+				>
+			</li>
+		{/each}
+	</ul>
+</section>
