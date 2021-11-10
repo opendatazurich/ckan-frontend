011f43d684831ea654b94a248e5419946958b3af
Sascha Aeppli <sascha.aeppli@liip.ch>
2021-11-10 20:37:41 +0100

    extract header and footer. nav toggle.

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,38 @@
+<footer class="site-footer">
+	<div class="container">
+		<nav class="footer-links-left row-fluid">
+			<ul class="unstyled row-fluid">
+				<li>
+					<a href="http://www.stadt-zuerich.ch/portal/de/index/ogd/nutzungsbedingungen.html"
+						>Nutzungsbedingungen</a
+					>
+				</li>
+				<li><a href="http://stadt-zuerich.ch/portal/de/index/ogd/kontakt.html">Kontakt</a></li>
+				<li>
+					<a href="https://github.com/opendatazurich" target="_blank">Quellcode auf GitHub</a>
+				</li>
+			</ul>
+		</nav>
+		<nav class="footer-links-right row-fluid">
+			<ul class="unstyled row-fluid">
+				<li><a href="http://ckan.org/" target="_blank">Entwickelt mit CKAN</a></li>
+				<li>
+					<a href="http://docs.ckan.org/en/latest/api/index.html" target="_blank">CKAN API</a>
+				</li>
+				<li><a href="https://okfn.org/" target="_blank">Open Knowledge</a></li>
+			</ul>
+		</nav>
+	</div>
+</footer>
+<footer class="stadtzh-footer">
+	<div class="container">
+		<a class="logo" href="http://www.stadt-zuerich.ch/"
+			><img
+				class="logo"
+				src="/logo@2x.png"
+				alt="Webseite der Stadt Zürich"
+				title="Webseite der Stadt Zürich"
+			/></a
+		>
+	</div>
+</footer>
