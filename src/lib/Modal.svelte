c173e246069a9d392cbad366faaccc1c69840aa4
Sascha Aeppli <sascha.aeppli@liip.ch>
2022-01-31 08:58:01 +0100

    Modal

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,21 @@
+<div class="dialog" data-modal="dialog">
+	<div class="dialog_overlay" tabindex="-1">
+		<div style="display: inline-block; height: 100%; vertical-align: middle;" />
+		<div class="dialog_content_wrapper">
+			<div
+				class="dialog_content"
+				aria-labelledby="dialogTitle"
+				aria-describedby="dialogDescription"
+				role="dialog"
+			>
+				<slot />
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.dialog_content {
+		max-width: inherit;
+	}
+</style>
