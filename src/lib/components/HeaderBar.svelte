e7b4ae6700e050594c6e0d86e6699b4967264e4a
Rae Knowler <rae.knowler@liip.ch>
2024-06-11 16:05:22 +0200

    feat: Add survey link in logo header

--- src/lib/components/HeaderBar.svelte
+++ src/lib/components/HeaderBar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import {surveyLink} from "$lib/config";
 	import BurgerIcon from './BurgerIcon.svelte';
 	import MetaNav from './MetaNav.svelte';
 	import { navigating } from '$app/stores';
@@ -27,6 +28,7 @@
 	class="mod_header mod_header--bar"
 	class:hide_logo={hideLogo}
 	class:is_sticky_topbar={isSticky}
+	class:contains_survey_link={surveyLink}
 >
 	<div class="mod_header__topbar" class:is_not_sticky={!isSticky && mounted}>
 		<div class="mod_header__topbar-inner">
@@ -64,6 +66,14 @@
 					<img class="stage-color" src="/logo.svg" alt="Logo Stadt Zürich" />
 				</a>
 			</div>
+
+			{#if surveyLink}
+				<div class="stage-color mod_header__logobar-logo">
+					<a href="{surveyLink}" class="stage-color mod_logo mod_logo--in-bar">
+						Wie gefällt dir unser Datenkatalog? - Mach mit bei unser kurzen Befragung
+					</a>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
@@ -74,5 +84,15 @@
 	}
 	.stage-color {
 		background-color: var(--color-stage);
+	}
+	.contains_survey_link .mod_header__logobar-logo {
+		height: auto;
+		flex-wrap: wrap;
+	}
+
+	.contains_survey_link .mod_header__logobar-inner,
+	.contains_survey_link .mod_header__topbar-inner {
+		flex-wrap: wrap;
+		display: flex;
 	}
 </style>
